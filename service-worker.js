8c3627212e64c83d8c725add6bc4d92dc5c09abb
vfaraji89 <vfaraji89@gmail.com>
2026-02-19 01:17:16 +0300

    redesign: tokalator theme, pixel hover signals, CV content, download button

--- service-worker.js
+++
@@ -1,9 +0,0 @@
-// This is a minimal service worker. 
-// In a real-world scenario, you would add caching strategies here.
-self.addEventListener('install', (event) => {
-  console.log('Service Worker installing.');
-});
-
-self.addEventListener('fetch', (event) => {
-  // Offline functionality would be handled here.
-});
